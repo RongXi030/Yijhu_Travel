fc3bd3a8b7f9244a1231dda1500b775bbb297d52
rongxi <rongxi0505@gmail.com>
2026-01-04 14:49:40 +0800

    add hero banner

---
+++ JS/carousel.js
@@ -0,0 +1,39 @@
+const imageSection = document.getElementById('imageSection');
+const videoSection = document.getElementById('videoSection');
+const myVideo = document.getElementById('introVideo');
+const btnImage = document.getElementById('btnImage');
+const btnVideo = document.getElementById('btnVideo');
+
+function switchTo(mode) {
+    if (mode === 'image') {
+        imageSection.classList.remove('d-none');
+        videoSection.classList.add('d-none');
+        
+        myVideo.pause();
+
+        btnImage.classList.add('active');
+        btnVideo.classList.remove('active');
+
+    } else if (mode === 'video') {
+        imageSection.classList.add('d-none');
+        videoSection.classList.remove('d-none');
+        
+        myVideo.currentTime = 0;
+        myVideo.play();
+
+        btnImage.classList.remove('active');
+        btnVideo.classList.add('active');
+    }
+}
+
+myVideo.addEventListener('ended', () => {
+    console.log("影片播完了，自動切換到圖片...");
+    switchTo('image');
+});
+
+
+window.addEventListener('load', () => {
+    myVideo.play().catch(error => {
+        console.log("自動播放被瀏覽器擋住了，可能是因為沒有靜音", error);
+    });
+});
